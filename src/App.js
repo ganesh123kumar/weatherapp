d05d84ea85340bb42c13af8ed653e24aeccc2b15
Ganesh Kumar <104543129+ganesh123kumar@users.noreply.github.com>
2022-07-08 16:44:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{((data.main.temp.toFixed()-32)*0.55).toFixed(0)  } °C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -52,7 +52,7 @@ function App() {
         {data.name !== undefined &&
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              {data.main ? <p className='bold'>{((data.main.temp.toFixed()-32)*0.55).toFixed(0)  } °C</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
@@ -60,7 +60,7 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind ? <p className='bold'>{(data.wind.speed.toFixed()*1.609).toFixed(0)} KPH</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
